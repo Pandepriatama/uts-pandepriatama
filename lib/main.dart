b7db005684b7f89210eebf17a8c84ece2a935cff
Pandepriatama <putupande2003@gmail.com>
2024-04-04 04:23:03 +0800

    Membuat bagian body yg berisi gambar dan 5 button

--- lib/main.dart
+++ lib/main.dart
@@ -26,6 +26,40 @@ class HomePage extends StatelessWidget {
         title: Text('Judul'),
         centerTitle: true,
       ), 
+      body: ListView(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Image.network("https://i0.wp.com/umsu.ac.id/berita/wp-content/uploads/2023/08/laporan-keuangan-pengertian-fungsi-jenis-dan-format.jpg?fit=1000%2C597&ssl=1"),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('tombol 1'),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('tombol 2'),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('tombol 3'),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('tombol 4'),
+              ),
+              ElevatedButton(
+                onPressed: () {},
+                child: Text('tombol 5'),
+              ),
+            ],
+          ),
+          SizedBox(height: 200), // Menambahkan ruang 200 piksel di bawah tombol-tombol
+        ],
+      ),
     );
   }
 }
